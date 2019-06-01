4b8480823674fa9f12352ec2912fa212a293ac6e
marciock <marciockalves@gmail.com>
2019-06-01 17:14:56 -0300

    recomecando

--- database/models/permitions.js
+++
@@ -1,9 +0,0 @@
-module.exports=(sequelize,DataTypes)=>{
-
-  const permition=sequelize.define('permition',{
-      sector:DataTypes.INTEGER,
-      menu:DataTypes.INTEGER,
-      active:DataTypes.BOOLEAN
-  });
-   return permition;
-}
--- database/models/sectors.js
+++
@@ -1,9 +0,0 @@
-module.exports=(sequelize,DataTypes)=>{
-
-  const sector=sequelize.define('sector',{
-      sector:DataTypes.STRING,
-      description:DataTypes.STRING,
-      active:DataTypes.BOOLEAN
-  });
-   return sector;
-}
