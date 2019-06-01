4fe4b1417b144812e1de93fe850eafbb6b1a40a4
marciock <marciockalves@gmail.com>
2019-06-01 19:39:10 -0300

    new laoyut

--- routes/waiter/index.js
+++ routes/waiter/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const menuhome=require('../lib/menuhome');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(menuhome)
-  res.render('layouts/home',{menus:menuhome});
+ 
+  res.render('waiter/index');
 });
 
 module.exports = router;
