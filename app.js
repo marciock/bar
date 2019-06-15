7f95011f11b1a459a76d9c5bcd9a40970a6a2743
marciock <marciockalves@gmail.com>
2019-06-15 11:03:55 -0300

    materialize

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(expressLayouts);
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
